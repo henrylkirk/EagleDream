8d2ac40d94523f6a7dbfc6da2c9e9bd6dc751d45
henrylkirk <henry@the-kirks.com>
2017-12-05 14:06:09 -0500

    Removed unused font awesome and jquery

--- gulpfile.js
+++ gulpfile.js
@@ -70,22 +70,6 @@ gulp.task('copy', function() {
       '!**/*.map'
     ])
     .pipe(gulp.dest('vendor/bootstrap'))
-
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('vendor/jquery'))
-
-  gulp.src(['node_modules/jquery.easing/*.js'])
-    .pipe(gulp.dest('vendor/jquery-easing'))
-
-  gulp.src([
-      'node_modules/font-awesome/**',
-      '!node_modules/font-awesome/**/*.map',
-      '!node_modules/font-awesome/.npmignore',
-      '!node_modules/font-awesome/*.txt',
-      '!node_modules/font-awesome/*.md',
-      '!node_modules/font-awesome/*.json'
-    ])
-    .pipe(gulp.dest('vendor/font-awesome'))
 })
 
 // Default task
