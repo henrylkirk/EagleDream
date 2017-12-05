b2fc8f9e406ddf2679a75ff2a111d80435eb30e0
henrylkirk <henry@the-kirks.com>
2017-12-05 14:03:24 -0500

    Added faded border effect, fixed column spacing issue

--- js/app.js
+++ js/app.js
@@ -15,24 +15,21 @@ var app = new Vue({
     character: {
       name: "Regex",
       realm: "Dalaran"
-    },
-    base_url: "https://us.api.battle.net/wow",
-    api_key: "czfaxfymsqtcf84fdy7f784qkpfxjkh3"
+    }
 },
     filters: {
-        percent: function (value) {
-            if (!value) return ''
+        percent: function (value) { // Display a number as a percent
             value = value.toFixed(2)
             return value+"%"
         },
-        capitalize: function (value) {
+        capitalize: function (value) { // Capitalize string
             if (!value) return ''
             value = value.toString()
             return value.charAt(0).toUpperCase() + value.slice(1)
         }
     },
     methods: {
-      search: function() {
+      search: function() { // Get data from Blizzard API
           let url = buildUrl(`character/${this.character.realm}/${this.character.name}`);
           axios.get(url,{
               params: {
@@ -47,24 +44,16 @@ var app = new Vue({
               console.log(this.items);
               console.log(this.stats);
               this.open();
-
-              // key item type
-              let keys = Object.keys(response.data.items);
-              console.log(keys);
-              this.itemTypes = keys;
-              console.log(this.itemTypes);
-              console.log(this.itemTypes);
           })
           .catch((error) => {
               console.log(error);
           });
       },
-      close: function() {
+      close: function() { // Close character area
           var characterArea = document.getElementById("character-area");
-          // characterArea.style.display = characterArea.style.display === 'none' ? '' : 'none';
           characterArea.style.display = 'none';
       },
-      open: function() {
+      open: function() { // Open character area
           var characterArea = document.getElementById("character-area");
           characterArea.style.display = '';
       }
